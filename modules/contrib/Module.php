75cf456873f16596ed398c3e4f2251b11984eef8
Huynh Minh Tri <huynhminhtri2412@gmail.com>
2021-01-07 16:28:24 +0700

    update

---
+++ modules/contrib/Module.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace app\modules\contrib;
+
+/**
+ * app module definition class
+ */
+class Module extends \yii\base\Module
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
